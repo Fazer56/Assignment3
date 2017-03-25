3fa1717b62263ec08f9ea5e6c6d7869b5d0b24d7
Fazer56 <faoilean56@gmail.com>
2017-03-25 15:09:39 +0000

    Adding about page

--- charitysite/myApp/urls.py
+++ charitysite/myApp/urls.py
@@ -2,6 +2,9 @@
 from django.conf.urls import url, include
 from . import views
 
+
+
 urlpatterns = [
-    url(r'^$', views.index, name='index'), #return file 'views' function 'index'
+    url(r'^$', views.appView.index, name='index'),#return file 'views' function'index'
+    url(r'^about/', views.appView.about, name='about'), 
 ]
