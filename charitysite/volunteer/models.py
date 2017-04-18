4b2c7bdc277c76b173cd0aa4da48f04205966df7
Fazer56 <faoilean56@gmail.com>
2017-04-18 15:06:46 +0100

    Adding to the database

--- charitysite/volunteer/models.py
+++ charitysite/volunteer/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Volunteer(models.Model):
+class Member(models.Model):
     first_name = models.CharField(max_length=50)
     surname =  models.CharField(max_length=50)
     address = models.CharField(max_length =150, blank=True)
