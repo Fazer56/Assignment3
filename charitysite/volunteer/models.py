6896a4140b6bd2ebc3cf105fdddc152829283cb2
Fazer56 <faoilean56@gmail.com>
2017-04-18 16:42:49 +0100

    Database for volunteering locations

--- charitysite/volunteer/models.py
+++ charitysite/volunteer/models.py
@@ -1,3 +1,4 @@
+from pygeocoder import Geocoder
 from django.db import models
 
 class Member(models.Model):
@@ -8,3 +9,17 @@ class Member(models.Model):
     
     def __str__(self):
         return self.first_name + ' - ' + self.surname
+
+class Location(models.Model):
+    building_name = models.CharField(max_length=50, blank = True)
+    street_address= models.CharField(max_length=50)
+    locality =models.CharField(max_length=50, blank = True)
+    city= models.CharField(max_length=50)
+    county =models.CharField(max_length=50)
+    latitude = models.FloatField(max_length=30, blank = True)
+    longitude = models.FloatField(max_length=30, blank = True)
+    
+    def __str__(self):
+        return self.street_address + ', ' + self.city + ', ' + self.county 
+    
+    
