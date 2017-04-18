0ef7b869f1383780ef36de8dab185d420523dc0c
Fazer56 <faoilean56@gmail.com>
2017-04-18 15:15:08 +0100

    Added function for displaying members

--- charitysite/volunteer/models.py
+++ charitysite/volunteer/models.py
@@ -6,4 +6,5 @@ class Member(models.Model):
     address = models.CharField(max_length =150, blank=True)
     email = models.EmailField(max_length=100)
     
-    
+    def __str__(self):
+        return self.first_name + ' - ' + self.surname
